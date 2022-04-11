649295958375f24f050cd546e03a02d9e4866263
Shad <shadwritescode@gmail.com>
2022-04-11 15:34:04 -0600

    some unneccesary migrations for the last fix.

---
+++ Migrations/20220410060725_fix_prop.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace bookstore.Migrations
+{
+    public partial class fix_prop : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
